fe132d495a7bb60b52e964a9e70140ad2c121fb1
mbnmoeini <mbn.moeini@gmail.com>
2024-10-23 11:04:12 +0330

    Update model module by adding regularization

--- model.py
+++ model.py
@@ -1,25 +1,26 @@
 import tensorflow as tf
 from keras import models, layers
 
-def create_model(input_shape):
+def create_model(input_shape, regularization=None):
     model = models.Sequential()
 
     # Input layer
-    model.add(layers.Conv2D(32, (3, 3), activation= 'relu', input_shape=input_shape))
+    model.add(layers.Conv2D(32, (3, 3), activation= 'relu', input_shape=input_shape, kernel_regularizer=regularization))
     model.add(layers.MaxPooling2D((2,2)))
 
+
     # Layer 2
-    model.add(layers.Conv2D(64, (3, 3), activation= 'relu'))
+    model.add(layers.Conv2D(64, (3, 3), activation= 'relu', kernel_regularizer=regularization))
     model.add(layers.MaxPooling2D(2, 2))
 
     # Layer 3
-    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu', kernel_regularizer=regularization))
 
     # Flatten the output from the convolutional layers
     model.add(layers.Flatten())
 
     # Dense layer with Dropout for regularization
-    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dense(128, activation='relu', kernel_regularizer=regularization))
     model.add(layers.Dropout(0.25))
 
     # Output layer for 10 classes
